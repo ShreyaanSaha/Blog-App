e4fc7591add9fa246d06fe2a8cf34fede45324b8
shreyaan <shreyaan.saha.1009@gmail.com>
2024-02-04 02:25:00 +0530

    Created SignUp API route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,8 +10,11 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
