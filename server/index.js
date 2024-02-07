a2839ee86a88699c24bb163f4a1cbbc04ab4ec87
shreyaan <shreyaan.saha.1009@gmail.com>
2024-02-07 19:07:11 +0530

    Added Update user api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
