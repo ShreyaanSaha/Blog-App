4282c7bc343f1c009124ad75d6a4cca23096318e
shreyaan <shreyaan.saha.1009@gmail.com>
2024-02-15 15:33:04 +0530

    Added Posts section to the dashboard

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
