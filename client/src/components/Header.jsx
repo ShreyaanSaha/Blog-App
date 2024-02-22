1359f2c0265a6e4dfc8654360dd61328a18cad8d
shreyaan <shreyaan.saha.1009@gmail.com>
2024-02-23 00:33:04 +0530

    Completed About and Projects pages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -54,10 +54,10 @@ export default function Header() {
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Sahand's
+        <span className='px-2 py-1 bg-gradient-to-r from-blue-500 via-cyan-500 to-green-300 text-white'>
+          Digi
         </span>
-        Blog
+        Chronicles
       </Link>
       <form onSubmit={handleSubmit}>
         <TextInput
@@ -103,7 +103,7 @@ export default function Header() {
           </Dropdown>
         ) : (
           <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline>
+            <Button gradientDuoTone='greenToBlue' outline>
               Sign In
             </Button>
           </Link>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About DigiChronicles
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to DigiChronicles, the brainchild of Shreyaan, a passionate explorer of technology and coding. This digital haven offers weekly insights into web development, software engineering, and programming languages, reflecting Shreyaan's commitment to continuous learning and embracing new technologies.
+            </p>
+
+            <p>
+              DigiChronicles is your go-to for staying updated on the ever-evolving digital landscape. Shreyaan's dedication ensures a constant stream of fresh content, making it a must-visit for those seeking the latest in tech trends.
+            </p>
+
+            <p>
+              Engagement is key at DigiChronicles – share your thoughts in the comments, like and reply to fellow readers. Join Shreyaan and the community on this digital journey, where the world of technology unfolds with each weekly post.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
